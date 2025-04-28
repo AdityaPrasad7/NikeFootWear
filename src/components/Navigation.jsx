cb56a56a27688db5d21b6da4c86533d113b6bbe3
Aditya <aditya@gmail.com>
2025-04-28 15:46:47 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () =>{
+    return(
+        <div>
+        <nav className="container">
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="brand"/>
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">contact</li>
+            </ul>
+            <button>login</button>
+        </nav>
+    </div>
+    );
+};
+export default Navigation;
